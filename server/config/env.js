589d448604dacdfa7ed4c91fbaa70c4fc6d3ee39
Pradeep <pradeep99909@gmail.com>
2024-07-02 23:47:26 +0530

    env added

--- server/config/env.js
+++ server/config/env.js
@@ -1,6 +1,9 @@
 const VARIABLE = {
   PORT: process.env.PORT,
   SPOTIFY_URL: process.env.SPOTIFY_URL,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REDIRECT_URL: process.env.REDIRECT_URL,
 };
 
 module.exports = {
